57a78fec5453a016f7d9f26cb7f9c32b007e1c38
Nation47 <hananiyusuph98@gmail.com>
2023-10-04 17:36:22 +0300

    chang

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -2,8 +2,10 @@ const HomePage = () => {
     return (
         <>
             <div className="bg-gray-300">
-                <div className="bg-white shadow-md p-4">
-                    <h2>Home</h2>
+                <div className="bg-white shadow-md p-4 text-sm flex">
+                    <h2 className="mr-1">Home /</h2>
+                    <p className="text-gray-500 mr-1">Student Profile / </p>
+                    <p className="text-gray-500">2021-04-00075 | ABDALLAH, HANANI Y</p>
                 </div>
             </div>
         </>
