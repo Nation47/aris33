912e2ec59ba5ace723e192ca7ac5c1c395c03a56
Nation47 <hananiyusuph98@gmail.com>
2023-10-04 18:51:22 +0300

    chng

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -1,5 +1,6 @@
 import { HiBookOpen, HiMenu } from "react-icons/hi";
 import { FaMasksTheater, FaReadme } from "react-icons/fa6";
+import Link from "next/link";
 
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
                     <p className="text-gray-500 mr-1">Student Profile / </p>
                     <p className="text-gray-500">2021-04-00075 | ABDALLAH, HANANI Y</p>
                 </div>
+
                 <div className="bg-white shadow-md m-6">
                     <div className="border-b-2 border-gray-300">
                         <img src="" alt="hanan" />
@@ -22,6 +24,13 @@ const HomePage = () => {
                         <li className="text-gray-400 flex items-center"> <FaReadme className="mr-1" />1st Year(2021/22)</li>
                         <li className="text-green-800 font-semibold flex items-center"><FaReadme className="mr-1" />2nd Year(2022/23)</li>
                     </ol>
+                </div>
+                <div className="bg-white shadow-md m-6 p-4">
+                    <h1 className="text-green-800 text-2xl font-semibold flex items-center">
+                        <FaReadme className="mr-1" />
+                        2nd Year(2022/23)
+                    </h1>
+                    <Link href="/" className="mt-5 text-blue-700 text-sm">My Courses & Results</Link>
                 </div>
             </div>
         </>
