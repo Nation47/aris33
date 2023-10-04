7afdf126774608922bd3f4d5758eb48dcfef7ca7
Nation47 <hananiyusuph98@gmail.com>
2023-10-04 18:27:24 +0300

    alot

---
+++ app/components/Header.jsx
@@ -0,0 +1,13 @@
+import { HiBookOpen, HiMenu } from "react-icons/hi";
+
+const Header = () => {
+    return (
+        <>
+            <div className="bg-blue-600 text-white flex justify-end p-4">
+                <HiMenu />
+            </div>
+        </>
+    );
+}
+ 
+export default Header;
