d68bf28a96059de28eea96910b5fa190acded149
Nation47 <hananiyusuph98@gmail.com>
2023-10-04 18:36:09 +0300

    chng

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import HomePage from './components/HomePage'
+import Header from './components/Header'
 
 export default function Home() {
   return (
     <main className="">
+      <Header />
         <HomePage />
     </main>
   )
